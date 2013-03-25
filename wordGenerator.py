db624e885c499219bd08e5a93a37825b9fc0b565
RedSpaceman <samuel.corcoran+github@gmail.com>
2013-03-25 23:40:23 +0000

    Switched to alternating between vowel/consonant phonemes, as two vowel phonemes in a row does not make linguistic sense Reduced max word length

--- wordGenerator.py
+++ wordGenerator.py
@@ -4,6 +4,7 @@
 # Control by phoneme:
 #  How rounded the phoneme is? How sharp? Short? Upwards inflexion, downwards inflexion?
 #  http://www.lancsngfl.ac.uk/curriculum/literacy/lit_site/lit_sites/phonemes_001/
+#  phoneme freq: http://myweb.tiscali.co.uk/wordscape/wordlist/phonfreq.html
 
 import random
 import string
@@ -24,10 +25,10 @@ totalWords = 10
 print "Config:"
 minWordLength = 3
 print "Minimum word length: " + str(minWordLength)
-maxWordLength = minWordLength + random.randint(0, 6)
+maxWordLength = minWordLength + random.randint(0, 5)
 print "Maximum word length: " + str(maxWordLength)
-vowelProbability = random.random()
-print "Vowel probability: " + str(vowelProbability)
+vowelProbability = 0.5
+print "Starting vowel probability: " + str(vowelProbability)
 
 print "\nGenerated words: " 
 for i in range(totalWords):
@@ -41,32 +42,31 @@ for i in range(totalWords):
     symbolString = ""
     finishedWord = ""
 
+    wasVowel = False
+    randomNum = random.random()
+    if randomNum <= 0.5:
+        wasVowel = True
+
     for x in range(wordLength):
-        # Choose a vowel or consonant
-        randomNum = random.random()
-
-        # Tip probabilities based on streaks (e.g. vowel increasingly likely after consonant streak)
-        randomNum -= consonantStreak * 0.1
-        randomNum += vowelStreak * 0.1
-
-        if randomNum <= vowelProbability:
+        # Alternate between vowel phonemes and consonant phonemes
+        if wasVowel == False:
+            #print "Chose vowel"
             # Choose a vowel
             symbolString += "v"
             vowelsIndex = random.randint(0, len(vowelPhonemes)-1)
             ##print "Vowel chosen (" + str(vowelsIndex) + ": " + vowelPhonemes[vowelsIndex] + ")" 
             finishedWord += vowelPhonemes[vowelsIndex]
             # Continue vowel streak/break consonant streak
-            vowelStreak += 1
-            consonantStreak = 0
+            wasVowel = True
         else:
+            #print "Chose consonant"
             # Choose a consonant
             symbolString += "c"
             consonantsIndex = random.randint(0, len(consonantPhonemes)-1)
             ##print "Consonant chosen (" + str(consonantsIndex) + ": " + consonantPhonemes[consonantsIndex] + ")" 
             finishedWord += consonantPhonemes[consonantsIndex]
             # Continue vowel streak/break consonant streak
-            consonantStreak += 1
-            vowelStreak = 0
+            wasVowel = False
 
     finishedWord = string.capitalize(finishedWord)
     # Print finished word
