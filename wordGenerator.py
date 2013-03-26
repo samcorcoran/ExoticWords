ebce1645ab524f382f5cbd7a3c94b3998ae33432
RedSpaceman <samuel.corcoran+github@gmail.com>
2013-03-26 23:46:33 +0000

    This version assigns phonemes with random probabilities which biases their selection, making some phonemes less common than others. Vowels stored in a dict as keys, with random probabilities stored as their values. Values across all vowels are normalised. Once vowel is desired, a random value is generated and the cumulative probabilities of the vowel set are traversed as a CDF until vowel is selected Same approach is performed for consonant phonemes too

--- wordGenerator.py
+++ wordGenerator.py
@@ -14,15 +14,44 @@ import string
 print 'Word Generator!\n'
 
 
-vowels = ['a', 'e', 'i', 'o', 'u']
-consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+vowelsList = ['a', 'e', 'i', 'o', 'u']
+consonantsList = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+
+
+
 
 vowelPhonemes = ['a', 'e', 'i', 'o', 'u', 'ae', 'ee', 'ie', 'oe', 'ue', 'oo', 'ar', 'ur', 'or', 'au', 'er', 'ow', 'oi', 'air', 'ear']
 # Note: This contains graphemes which are not distinct phonemes, such as "c" which shares the phoneme "k"
 consonantPhonemes = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', 'wh', 'th', 'ch', 'sh', 'zh', 'ng']
 
+## Vowel Probabilities
+vowelProbs = dict()
+# Assign a probability to each vowelPhoneme
+totalVowelProbs = 0
+for nextPhoneme in vowelPhonemes:
+    vowelProbs[nextPhoneme] = random.uniform(0.5, 1)
+    totalVowelProbs += vowelProbs[nextPhoneme]
+# Normalise vowel probabilities
+for vowel in vowelProbs:
+    vowelProbs[vowel] /= totalVowelProbs
+#print vowelProbs
+
+## Consonant Probabilities
+consonantProbs = dict()
+# Assign a probability to each vowelPhoneme
+totalConsonantProbs = 0
+for nextPhoneme in consonantPhonemes:
+    consonantProbs[nextPhoneme] = random.random()
+    totalConsonantProbs += consonantProbs[nextPhoneme]
+# Normalise consonant probabilities
+for cons in consonantProbs:
+    consonantProbs[cons] /= totalVowelProbs
+#print consonantProbs
+
+
+
 # How many words should be generated in this batch?
-totalWords = 10
+totalWords = 20
 
 # How should the process be configured?
 print "Config:"
@@ -32,9 +61,9 @@ maxWordLength = minWordLength + random.randint(0, 5)
 print "Maximum word length: " + str(maxWordLength)
 vowelProbability = round(random.uniform(0.4, 0.6), 2)
 print "Vowel probability: " + str(vowelProbability) + " (0.4 to 0.6)"
-streakModifier = round(random.uniform(0.35, 0.5), 2)
-print "Streak modifier: " + str(streakModifier) + " (larger modifiers decrease the likelihood of double-selections)"
+streakModifier = round(random.uniform(0.2, 0.5), 2)
 
+print "Streak modifier: " + str(streakModifier) + " (larger modifiers decrease the likelihood of double-selections)"
 
 print "\nGenerated words: "
 for i in range(totalWords):
@@ -62,28 +91,50 @@ for i in range(totalWords):
         # Alternate between vowel phonemes and consonant phonemes
         if vowelChoiceRand < vowelProbability:
             #print "Chose vowel"
-
             # Choose a vowel
             symbolString += "v"
-            vowelsIndex = random.randint(0, len(vowelPhonemes)-1)
+            
+            chosenVowel = ""
+            # Generate a random to sample from vowel pdf
+            vSample = random.random()
+            cumVowelProbs = 0
+            # Move through vowel probs, checking if sample is within interval
+            for vowel in vowelProbs:
+                cumVowelProbs += vowelProbs[vowel]
+                if vSample < cumVowelProbs:
+                    # sample is within interval
+                    chosenVowel = vowel
+                    break
+                        
+            ##vowelsIndex = random.randint(0, len(vowelPhonemes)-1)
+
             ##print "Vowel chosen (" + str(vowelsIndex) + ": " + vowelPhonemes[vowelsIndex] + ")"
-            finishedWord += vowelPhonemes[vowelsIndex]
+            
+            finishedWord += chosenVowel
+
             # Continue vowel streak/break consonant streak
             wasVowel = True
-
             vowelStreak += 1
             consonantStreak = 0
         else:
             #print "Chose consonant"
-
             # Choose a consonant
             symbolString += "c"
-            consonantsIndex = random.randint(0, len(consonantPhonemes)-1)
-            ##print "Consonant chosen (" + str(consonantsIndex) + ": " + consonantPhonemes[consonantsIndex] + ")"
-            finishedWord += consonantPhonemes[consonantsIndex]
+
+            chosenConsonant = ""
+            # Generate a random to sample from consonant pdf
+            cSample = random.random()
+            cumConsProbs = 0
+            # Move through vowel probs, checking if sample is within interval
+            for cons in consonantProbs:
+                cumConsProbs += consonantProbs[cons]
+                if cSample < cumConsProbs:
+                    # sample is within interval
+                    chosenConsonant = cons
+                    break
+            finishedWord += chosenConsonant
             # Continue vowel streak/break consonant streak
             wasVowel = False
-
             consonantStreak += 1
             vowelStreak = 0
 
